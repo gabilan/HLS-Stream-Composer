7aaf41649af1a5b4e2deac3d9a198b1538fda1c5
Ernest Wilkerson <gabilan.roku@gmail.com>
2011-04-06 13:51:19 -0400

    added: native project for handling transcoding and segmentation

---
+++ HlsStreamComposer.Native/HlsStreamComposer.Native.cpp
@@ -0,0 +1,10 @@
+// HlsStreamComposer.Native.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "HlsStreamComposer.Native.h"
+
+HLSSTREAMCOMPOSERNATIVE_API int Initialize(void)
+{
+	return 0;
+}
---
+++ HlsStreamComposer.Native/HlsStreamComposer.Native.h
@@ -0,0 +1,14 @@
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the HLSSTREAMCOMPOSERNATIVE_EXPORTS
+// symbol defined on the command line. this symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// HLSSTREAMCOMPOSERNATIVE_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef HLSSTREAMCOMPOSERNATIVE_EXPORTS
+#define HLSSTREAMCOMPOSERNATIVE_API __declspec(dllexport)
+#else
+#define HLSSTREAMCOMPOSERNATIVE_API __declspec(dllimport)
+#endif
+
+HLSSTREAMCOMPOSERNATIVE_API int Initialize(void);
+
